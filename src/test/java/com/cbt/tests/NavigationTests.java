3bd21b22600a72f94f7c39393c6103bd09e84574
Bahar Taskin <cakirbhr7@gmail.com>
2023-01-26 23:52:46 +0300

    NavigationTests.java created

---
+++ src/test/java/com/cbt/tests/NavigationTests.java
@@ -0,0 +1,98 @@
+package com.cbt.tests;
+
+import com.cbt.utilities.BrowserFactory;
+import com.cbt.utilities.StringUtility;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class NavigationTests {
+
+    @Test
+    public void navigationTest_Chrome(){
+
+        WebDriver driver = BrowserFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+
+        driver.get("https://google.com");
+
+        String googleTitle = driver.getTitle();
+
+        driver.get("https://etsy.com");
+
+        String etsyTitle = driver.getTitle();
+
+        driver.navigate().back();
+
+        String googleTitle2 = driver.getTitle();
+
+        StringUtility.verifyEquals(googleTitle,googleTitle2);
+
+        driver.navigate().forward();
+
+        String etsyTitle2 = driver.getTitle();
+
+        StringUtility.verifyEquals(etsyTitle,etsyTitle2);
+
+
+
+
+    }
+
+    @Test
+    public void navigationTest_Firefox(){
+
+        WebDriver driver = BrowserFactory.getDriver("firefox");
+        driver.manage().window().maximize();
+
+        driver.get("https://google.com");
+
+        String googleTitle = driver.getTitle();
+
+        driver.get("https://etsy.com");
+
+        String etsyTitle = driver.getTitle();
+
+        driver.navigate().back();
+
+        String googleTitle2 = driver.getTitle();
+
+        StringUtility.verifyEquals(googleTitle,googleTitle2);
+
+        driver.navigate().forward();
+
+        String etsyTitle2 = driver.getTitle();
+
+        StringUtility.verifyEquals(etsyTitle,etsyTitle2);
+
+    }
+
+    @Test
+    public void navigationTest_Edge(){
+
+
+        WebDriver driver = BrowserFactory.getDriver("edge");
+        driver.manage().window().maximize();
+
+        driver.get("https://google.com");
+
+        String googleTitle = driver.getTitle();
+
+        driver.get("https://etsy.com");
+
+        String etsyTitle = driver.getTitle();
+
+        driver.navigate().back();
+
+        String googleTitle2 = driver.getTitle();
+
+        StringUtility.verifyEquals(googleTitle,googleTitle2);
+
+        driver.navigate().forward();
+
+        String etsyTitle2 = driver.getTitle();
+
+        StringUtility.verifyEquals(etsyTitle,etsyTitle2);
+
+    }
+
+}
